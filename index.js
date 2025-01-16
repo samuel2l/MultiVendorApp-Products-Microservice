7b86f9d917d16cb8a1142c0c359f04ecda6a783f
Samuel Adams <sama29571@gmail.com>
2025-01-16 13:41:42 +0000

    fix cors for mobile

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const cors = require("cors");
 const port=process.env.PORT||8002
 const productRoutes = require("./api/products");
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://multivendorplatform-shopping-frontend.onrender.com'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use();
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'https://multivendorplatform-shopping-frontend.onrender.com'],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
 
 app.use(express.static(__dirname + "/public"));
 
